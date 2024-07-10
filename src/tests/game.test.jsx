6e98f4d2ccb12e1b590afe302bca180462bb87eb
Morgan Page <morganjpage@gmail.com>
2024-07-11 00:00:17 +0100

    More test amends

--- src/tests/game.test.jsx
+++ src/tests/game.test.jsx
@@ -3,19 +3,8 @@ import { Actor } from "@dfinity/agent";
 import { actor,gameCanister } from "./gameActor";
 import { adminActor } from "./actor";
 
-
 test("Should be able to click", async () => {
-  //Get the principal of the clicker-game and add it to the admins
-  //let clickGamePrincipal = Actor.canisterIdOf(actor);
-  //console.log(clickGamePrincipal.toText());
-  //const result2 = await adminActor.addAdmin(clickGamePrincipal);
-  //console.log(result2);
-  // const result2 = await adminActor.createAchievement("Clicker Game", "Test Achievement", "Test Achievement Description", 5, false, false);
-  // console.log(result2);
-
-
   const result1 = await actor.click();
   console.log(result1);
-
   expect(result1.ok).not.toBe(null);
 });
